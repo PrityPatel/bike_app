2f8852c33abc0415ef4eca018cb64276cd2b1616
Prity Patel <pritypatel714@gmail.com>
2015-07-23 16:13:19 -0700

    updating with views and login

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -3,11 +3,27 @@ class BicyclesController < ApplicationController
   end
 
   def new
+    @bicycle = current_user.bicycles.new
   end
+
+  def create
+    @bicycle = current_user.bicycles.new(bicycle_params)
+    if @bicycle.save
+      redirect_to user_path(current_user)
+    else
+      render 'new'
+  end
+end
 
   def edit
   end
 
   def show
   end
+
+  private
+  def bicycle_params
+    params.require(:bicycle).permit(:desciption, :price, :url)
 end
+end
+
